6ba1d570a22fd6176b0a8afe514245d4927a79f0
lwp <liangwp@gmail.com>
2018-01-01 21:02:22 +0800

    websocket added

--- src/client/js/myscript.js
+++ src/client/js/myscript.js
@@ -1,10 +1,37 @@
 
 document.addEventListener('DOMContentLoaded', init); // works in chrome
-
 function init() {
-    var el = document.getElementById("renderCanvas");
+    
+    // set body.onload in script instead of in html doc
+    if (document.body) {
+        document.body.onload = function () {
+            brython();
+        }
+    }
+    
+    var el = document.getElementById("justatest");
     setTimeout(function () {
-        el.innerHTML = "timeout";
+        el.innerHTML = "timeout from javascript";
     }, 3000);
+    
+    window.test = function(value) {
+        console.log("test: " + value);
+    }
+    
+    doWebSocketTest();
 }
 
+function doWebSocketTest() {
+    //var socket = new WebSocket('ws://demos.kaazing.com/echo');
+    var socket = new WebSocket('ws://localhost:8082');
+    socket.addEventListener('error', function (event) {
+        console.log("an error occurred... duh");
+    });
+    socket.addEventListener('open', function (event) {
+        console.log("send message to server");
+        socket.send("hello server");
+    });
+    socket.addEventListener("message", function (event) {
+        console.log("Received from server: " + event.data)
+    });
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const http = require("http");
 const express = require("express");
+const WebSocket = require('uws')
+
 var app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({"host": server, "port": 8082}); // websocket server must use a different port from the http server
 
 var port = 8080;
 
@@ -9,6 +15,20 @@ app.use('/', express.static(__dirname+"/www"));
 
 app.get('/api', function (req, res) {
     res.send("API hello world");
+});
+
+wss.on('connection', function (ws, req) {
+    ws.on("message", function (message) {
+        console.log("server - received via websocket: " + message);
+        console.log("server - send reply");
+        ws.send("replied via websocket");
+    });
+});
+wss.on("listening", function () {
+    console.log("ws server is listening");
+})
+wss.on("error", function (err) {
+    console.log(err);
 });
 
 app.listen(port, function () {
